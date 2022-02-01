48506d17e93d1cf2b24d8d31afa17d656ae9865a
Ryan Fickenscher <rsficken@gmail.com>
2022-01-31 22:06:05 -0700

    common-collections

---
+++ common-collections/src/main.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+fn main() {
+    let text = "Hello world, wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+}
